4d05aff74dc279a9a4eebb341fa693e36c193baa
pcardoso-ufabc <p.cardoso@aluno.ufabc.edu.br>
2025-12-01 19:19:08 -0300

    Update operacoes.h

--- Main/operacoes.h
+++ Main/operacoes.h
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#define MAX 101
 
 typedef struct Node {
     int digito;
@@ -9,12 +8,13 @@ typedef struct Node {
 Node* criar_no(int dado);
 
 void ler_numero(char num[], int A[], int *tam, int *negativo);
-int soma(int A[], int tamA, int B[], int tamB, int S[]);
-int subtracao(int A[], int tamA, int B[], int tamB, int S[], int *neg);
+int soma(int A[], int tamA, int B[], int tamB, int S[], int MAX);
+int subtracao(int A[], int tamA, int B[], int tamB, int S[], int *neg, int MAX);
 int multiplicacao(int A[], int tamA, int B[], int tamB, int S[]);
 int maior_ou_igual(int A[], int tamA, int B[], int tamB);
-int divisao(int A[], int tamA, int B[], int tamB, int Q[], int R[], int *tamR);
+int divisao(int A[], int tamA, int B[], int tamB, int Q[], int R[], int *tamR, int MAX);
 void imprime_resultado(int S[], int digitos, int negativo);
+char* ler_string(int *MAX, int *tamanho);
 void imprimir_recursivo_aux(Node* no);
 void liberar_lista_aux(Node* head);
 void fatorial(int n);
